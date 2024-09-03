ea0eb0b39a80e5107a4ae31017cba6d70a5bdc7e
Buran11 <ionovv1985@gmail.com>
2024-09-03 19:09:33 +0700

    add module_2_2 in dir module_2

---
+++ module_2/module_2_2.py
@@ -0,0 +1,13 @@
+#2023/09/29 00:00|Домашняя работа по уроку "Условная конструкция. Операторы if, elif, else."
+
+print('ЗАДАЧА: ВСЕ ЛИ РАВНЫ?')
+print('Введите 3 целых числа.\n')
+first = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+third = int(input('Введите третье число: '))
+if first == second == third:
+    print(3)
+elif first == second or first == third or third == second:
+    print(2)
+else:
+    print(0)
