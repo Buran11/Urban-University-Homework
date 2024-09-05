ff227a4ccd2617b7a03daf6a0e183ccea65bc2c8
Buran11 <ionovv1985@gmail.com>
2024-09-05 18:49:45 +0700

    Add module_2_3.py with the solution of the problem

---
+++ module_2/module_2_3.py
@@ -0,0 +1,16 @@
+#2023/10/01 00:00|Домашняя работа по уроку "Стиль кода часть II. Цикл While."
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+count_index = 0
+while_decision = (count_index <= len(my_list))
+
+while while_decision:
+    if my_list[count_index] > 0:
+        print(my_list[count_index])
+        count_index += 1
+        continue
+    elif my_list[count_index] == 0:
+        count_index += 1
+        continue
+    else:
+        break
