c3aa7feff44b97df711bffc61e80d5ce2b2d06ba
Buran11 <ionovv1985@gmail.com>
2024-09-22 17:07:00 +0700

    add files: module1, module2, fake_math, true_math, module_4_1 -> *.py in dir module_4

---
+++ module_4/module_4_1.py
@@ -0,0 +1,13 @@
+# 2023/10/18 00:00|Домашняя работа по уроку "Модули и пакеты"
+
+from fake_math import divide
+from true_math import divide
+
+result1 = divide(69, 3)
+result2 = divide(3, 0)
+result3 = divide(49, 7)
+result4 = divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
