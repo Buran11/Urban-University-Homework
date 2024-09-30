38030488c11b406742c07e4f5e46212ca6a91b7c
Buran11 <ionovv1985@gmail.com>
2024-09-30 22:47:09 +0700

    Add module_5_3.py

---
+++ module_5/module_5_3.py
@@ -0,0 +1,110 @@
+# 2023/10/31 00:00|Домашняя работа по уроку "Перегрузка операторов."
+
+class House:
+    # Инструкция к module_5_1
+    def __init__(self, name, number_of_floor):
+        self.name = name
+        self.number_of_floor = number_of_floor
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floor or new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            for i in range(new_floor):
+                print(f'Этаж {i + 1}')
+
+    # Инструкция к module_5_2
+    def __len__(self):
+        return self.number_of_floor
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floor}'
+
+    # Инструкция к module_5_3
+    def __verify_data(self, other):
+        # Проверка атрибутов на тип данных
+        if not isinstance(other, (House, int)) and not isinstance(self, (House, int)):
+            raise TypeError(
+                'Атрибут "other" должен пренадлежать типу "House" или "int"')
+    # 1
+
+    def __eq__(self, other):
+        self.__verify_data(other)
+        if isinstance(other, int):
+            return self.number_of_floor == other
+        else:
+            return self.number_of_floor == other.number_of_floor
+
+    # 2
+
+    def __lt__(self, other):
+        self.__verify_data(other)
+        return self.number_of_floor < other.number_of_floor
+
+    def __le__(self, other):
+        self.__verify_data(other)
+        return self.number_of_floor <= other.number_of_floor
+
+    def __gt__(self, other):
+        self.__verify_data(other)
+        return self.number_of_floor > other.number_of_floor
+
+    def __ge__(self, other):
+        self.__verify_data(other)
+        return self.number_of_floor >= other.number_of_floor
+
+    def __ne__(self, other):
+        self.__verify_data(other)
+        return self.number_of_floor != other.number_of_floor
+
+    # 3
+    def __add__(self, value):
+        self.__verify_data(value)
+        return House(self.name, self.number_of_floor + value)
+
+    # 4
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2)  # __eq__
+
+h1 = h1 + 10  # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10  # __iadd__
+print(h1)
+
+h2 = 10 + h2  # __radd__
+print(h2)
+
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
+
+
+# Вывод на консоль:
+# Название: ЖК Эльбрус, кол-во этажей: 10
+# Название: ЖК Акация, кол-во этажей: 20
+# False
+# Название: ЖК Эльбрус, кол-во этажей: 20
+# True
+# Название: ЖК Эльбрус, кол-во этажей: 30
+# Название: ЖК Акация, кол-во этажей: 30
+# False
+# True
+# False
+# True
+# False
--- module_5/work_space_05.py
+++ module_5/work_space_05.py
@@ -12,8 +12,23 @@ class Human:
         self.age += 1
         print(f'Happy birthday {self.name}! You are {self.age} years old')
 
+    def __str__(self):  # магический метод перегрузка str
+        return f'Name: {self.name}'
+
     def __len__(self):
         return self.age
+
+    def __lt__(self, other):  # магический метод сравнения перегрузка Lower than
+        return self.age < other.age
+
+    def __gt__(self, other):  # магический метод сравнения перегрузка Greater than
+        return self.age > other.age
+
+    def __eq__(self, other):  # магический метод сравнения перегрузка Equal to
+        return self.age == other.age and self.name == other.name
+
+    def __bool__(self):  # магический метод перегрузка bool
+        return bool(self.age)
 
     def __del__(self):  # деструктор
         print(f'Good bye {self.name}')
@@ -21,10 +36,19 @@ class Human:
 
 den = Human('Denis', 22)  # экземпляр класса
 max = Human('Maxim', 22)
-# del den # удаление экземпляра
-max.birthday()
-print(len(den))
+a = 6
+print(den)  # str
 
+# max.name = 'Denis'
+# if den: # bool
+#     den.say_info()
+# print(den < max)  # False lt
+# del den # удаление экземпляра
+# max.birthday()
+# print(len(den)) # возвращаем количество лет
+# print(den < max)  # сравнение возраста True lt
+# print(den > max)  # gt
+# print(den == max)  # eq
 # den.say_info()
 # max.say_info()
 # den.age += 1
