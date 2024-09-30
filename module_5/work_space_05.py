9820718148f96861919d495e30745158368de5b1
Buran11 <ionovv1985@gmail.com>
2024-09-30 19:59:53 +0700

    Add module_5_2.py

--- module_5/work_space_05.py
+++ module_5/work_space_05.py
@@ -1,5 +1,6 @@
 class Human:
-    def __init__(self, name, age) -> None:  # Метод вызывается при создании экземпляра
+    # Метод вызывается при создании экземпляра. __init__ - магический метод, дандер метод
+    def __init__(self, name, age) -> None:
         self.name = name  # Атрибут Переменные экземпляра
         self.age = age
         self.say_info()
@@ -11,13 +12,24 @@ class Human:
         self.age += 1
         print(f'Happy birthday {self.name}! You are {self.age} years old')
 
+    def __len__(self):
+        return self.age
+
+    def __del__(self):  # деструктор
+        print(f'Good bye {self.name}')
+
 
 den = Human('Denis', 22)  # экземпляр класса
 max = Human('Maxim', 22)
+# del den # удаление экземпляра
 max.birthday()
+print(len(den))
 
 # den.say_info()
 # max.say_info()
 # den.age += 1
 # print(den.name, den.age)
 # print(max.name, max.age)
+
+if __name__ == '__main__':
+    pass
