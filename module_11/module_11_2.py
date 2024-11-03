df3e112e132cfa5afb64350339d8b90ad9df4808
Buran11 <ionovv1985@gmail.com>
2024-11-04 01:01:45 +0700

    Add module_11_2.py

--- module_11/module_11_2.py
+++ module_11/module_11_2.py
@@ -1,32 +1,23 @@
 # 2023/12/26 00:00|Домашнее задание по теме "Интроспекция"
-class Introspectable:
-    '''
-    Возвращает словарь с данными об объекте, включающий следующую информацию:
-    1 - Type: Тип объекта.
-    2 - Atributs: Атрибуты объекта.
-    3 - Methods: Методы объекта.
-    4 - Modules: Модуль, к которому объект принадлежит.    
-    '''
-
-    def __init__(self, object: object):
-        self.object = object
-        self.__resalt = {}
-        self.__introspection_info()
-
-    def __introspection_info(self):
-        from pprint import pprint
-        type_ = type(self.object)
-        atribut = dir(self.object)
-        method = dir(type_)
-        module = type_.__module__
-        self.__resalt = {'1 - Type': type_, '2 - Atributs': atribut,
-                         '3 - Methods': method, '4 - Modules': module}
-        pprint(self.__resalt)
+import inspect
+from pprint import pprint
 
 
-def main():
-    Introspectable(1)
+def introspection_info(obj: object):
+    atributs = []
+    methods = []
+    type_ = type(obj)
+    print(type_)
+    for atribut in dir(obj):
+        if not callable(getattr(obj, atribut)):
+            atributs.append(atribut)
+    for method in dir(obj):
+        if callable(getattr(obj, method)):
+            methods.append(method)
+    module = inspect.getmodule(obj)
+    resalt = {'1 - Type': type_, '2 - Atributs': atributs,
+              '3 - Methods': methods, '4 - Module': module}
+    print(resalt)
 
 
-if __name__ == '__main__':
-    main()
+introspection_info(42)
