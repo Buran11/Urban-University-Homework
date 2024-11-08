fa9fe40f3cda9ef4cbc0fe5276dc9a687effabe5
Buran11 <ionovv1985@gmail.com>
2024-11-08 23:00:08 +0700

    Add module_13_3.py

--- module_13/tl_ActavianMyBot.py
+++ module_13/tl_ActavianMyBot.py
@@ -8,18 +8,28 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 
 
 # Хэндлеры приёма сообщений
+# Реагирует на текст сообщения Urban и ff
+@dp.message_handler(text=['Urban', 'ff'])
 async def urban_message(message: types.Message):
     print('Urban message!')
+    # В ответ получаем сообщение Urban message!
+    await message.answer('Urban message!')
 
 
-@dp.message_handler(commands=['start'])  # Реагирует по командам через /
+# Реагирует по командам через /
+@dp.message_handler(commands=['start'])
 async def start_message(message: types.Message):
     print('Start message!')
+    # В ответ получаем сообщение Рады вас видеть в нашем боте!
+    await message.answer('Рады вас видеть в нашем боте!')
 
 
-@dp.message_handler()  # Реагирует на любое сообщение
+# Реагирует на любое сообщение
+@dp.message_handler()
 async def all_messages(message: types.Message):
     print('Мы получили сообщение!')
+    # echo bot В ответ получаем сообщение Ваше сообщение в верхнем регистре!
+    await message.answer(message.text.upper())
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
