51e332cfab33c16b1dd52e290a5fadc6884d2589
Buran11 <ionovv1985@gmail.com>
2024-11-15 00:41:22 +0700

    Add module_14_5.py, changed crud_functions.py and add table Users in Products.db

--- module_14/crud_functions.py
+++ module_14/crud_functions.py
@@ -14,6 +14,61 @@ def initiate_db():
     )
     ''')
 
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users (
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL
+    )
+    ''')  # MODULE_14_5
+    connection.commit()
+    connection.close()
+
+
+def is_included(username):  # MODULE_14_5
+    connection = sqlite3.connect('Products.db')
+    cursor = connection.cursor()
+
+    check_user = cursor.execute(
+        'SELECT * FROM Users WHERE username = ?', (username,))
+    if check_user.fetchone() is None:
+        connection.commit()
+        connection.close()
+        return False
+    else:
+        connection.commit()
+        connection.close()
+        return True
+
+
+def is_included_product(title):
+    connection = sqlite3.connect('Products.db')
+    cursor = connection.cursor()
+
+    check_product = cursor.execute(
+        'SELECT * FROM Products WHERE title = ?', (title,))
+    if check_product.fetchone() is None:
+        connection.commit()
+        connection.close()
+        return False
+    else:
+        connection.commit()
+        connection.close()
+        return True
+
+
+def add_user(username, email, age):  # MODULE_14_5
+    connection = sqlite3.connect('Products.db')
+    cursor = connection.cursor()
+
+    if is_included(username) == False:
+        cursor.execute(f'''
+                       INSERT INTO Users (username, email, age, balance) VALUES ('{username}', '{email}', '{age}', '{1000}')
+        ''')
+        connection.commit()
+
     connection.commit()
     connection.close()
 
@@ -22,9 +77,7 @@ def add_product(title, description, price):
     connection = sqlite3.connect('Products.db')
     cursor = connection.cursor()
 
-    check_product = cursor.execute(
-        'SELECT * FROM Products WHERE title = ?', (title,))
-    if check_product.fetchone() is None:
+    if is_included_product(title) == False:
         cursor.execute(f'''
                        INSERT INTO Products (title, description, price) VALUES ('{title}', '{description}', '{price}')
         ''')
@@ -41,5 +94,6 @@ def get_all_products():
     cursor.execute('SELECT * FROM Products')
     products = cursor.fetchall()
 
+    connection.commit()
     connection.close()
     return products
