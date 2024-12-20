f0b16b7020050cad4e63e0c20cd883e7d27daafd
Buran11 <ionovv1985@gmail.com>
2024-12-20 20:23:47 +0700

    Add module_17_5.py

--- module_17/app/routers/user.py
+++ module_17/app/routers/user.py
@@ -4,6 +4,7 @@ from app.backend.db_depends import get_db
 
 from typing import Annotated
 from app.models.user import User
+from app.models.task import Task
 from app.schemas import CreateUser, UpdateUser
 
 from sqlalchemy import select, insert, update, delete
@@ -31,6 +32,23 @@ async def user_by_id(db: Annotated[Session, Depends(get_db)], user_id: int):
     return user
 
 
+@router.get('/user_id/tasks')
+async def task_by_user_id(db: Annotated[Session, Depends(get_db)], user_id: int):
+    task = db.scalars(select(Task).where(Task.user_id == user_id)).all()
+    user = db.scalars(select(User).where(User.id == user_id)).first()
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='User was not found'
+        )
+    tasks = db.scalars(select(Task).where(Task.user_id == User.id)).all()
+    all_tasks = [task.id] + [i.id for i in tasks]
+    user_tasks = db.scalars(select(Task).where(
+        Task.user_id.in_(all_tasks))).all()
+    db.commit()
+    return user_tasks
+
+
 @router.post('/create')
 async def create_user(db: Annotated[Session, Depends(get_db)], create_user: CreateUser):
     user = db.scalars(select(User).where(
@@ -40,11 +58,12 @@ async def create_user(db: Annotated[Session, Depends(get_db)], create_user: Crea
             status_code=status.HTTP_404_NOT_FOUND,
             detail='User already exists'
         )
-    db.execute(insert(User).values(username=create_user.username,
-                                   firstname=create_user.firstname,
-                                   lastname=create_user.lastname,
-                                   age=create_user.age,
-                                   slug=slugify(create_user.username)))
+    db.execute(insert(User).values(
+        username=create_user.username,
+        firstname=create_user.firstname,
+        lastname=create_user.lastname,
+        age=create_user.age,
+        slug=slugify(create_user.username)))
     db.commit()
     return {
         'status_code': status.HTTP_201_CREATED, 'transaction': 'Successful'
@@ -82,6 +101,7 @@ async def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
             detail='User was not found'
         )
     db.execute(delete(User).where(User.id == user_id))
+    db.execute(delete(Task).where(Task.user_id == user_id))
     db.commit()
     return {
         'status_code': status.HTTP_200_OK,
