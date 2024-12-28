e572c891b8ec3e2154cbe845b1a00cbc34941c37
Buran11 <ionovv1985@gmail.com>
2024-12-28 09:42:05 +0700

    Add module_18_3

--- module_18/UrbanDjango/UrbanDjango/urls.py
+++ module_18/UrbanDjango/UrbanDjango/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin  # type: ignore
 from django.urls import path  # type: ignore
 from task2.views import func_view, ClassView
 from task3.views import cart, store, platform
+from task4.views import platform2, store2, cart2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +11,7 @@ urlpatterns = [
     path('platform/cart/', cart),
     path('platform/store/', store),
     path('platform/', platform),
+    path('platform2/cart2/', cart2),
+    path('platform2/store2/', store2),
+    path('platform2/', platform2),
 ]
--- module_18/UrbanDjango/task4/views.py
+++ module_18/UrbanDjango/task4/views.py
@@ -1,35 +1,37 @@
 from django.shortcuts import render  # type: ignore
 
-
 # Create your views here.
-def platform(request):
+
+
+def platform2(request):
     context = {
-        'title': 'Platform',
+        'title': 'Platform2',
         'header': 'Главная страница',
         'm_main': 'Главная',
         'm_store': 'Магазин',
         'm_cart': 'Корзина'
     }
-    return render(request, 'third_task/platform.html', context)
+
+    return render(request, 'fourth_task/platform2.html', context)
 
 
-def store(request):
+def store2(request):
     context = {
-        'title': 'Store',
+        'title': 'Store2',
         'header': 'Игры',
         'b_back': 'На главную',
         'first_game': 'The Elden Ring',
         'second_game': 'Tekkin 8',
         'third_game': 'Gran Turismo 7'
     }
-    return render(request, 'third_task/store.html', context)
+    return render(request, 'fourth_task/store2.html', context)
 
 
-def cart(request):
+def cart2(request):
     context = {
-        'title': 'Cart',
+        'title': 'Cart2',
         'header': 'Корзина',
         'catr_is_empty': 'Извините, ваша корзина пуста',
         'b_back': 'На главную'
     }
-    return render(request, 'third_task/cart.html', context)
+    return render(request, 'fourth_task/cart2.html', context)
