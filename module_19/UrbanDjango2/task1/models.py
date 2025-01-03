68c9fe0d26ddbc97454647e471f9856ef85382f8
Buran11 <ionovv1985@gmail.com>
2025-01-03 18:26:28 +0700

    Add module_19

---
+++ module_19/UrbanDjango2/task1/models.py
@@ -0,0 +1,25 @@
+from django.db import models  # type: ignore
+
+
+# Create your models here.
+class Buyer(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=30)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.IntegerField()
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Buyer)
+    DecimalField = models.DecimalField
+    BooleanField = models.BooleanField
+
+    def __str__(self):
+        return self.title
